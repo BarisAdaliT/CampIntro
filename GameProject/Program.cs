710ea5d9d92343c0a2feefb6c72764ca547cd29b
BarisAdaliTurkmen <tbarisadali@gmail.com>
2021-08-14 18:04:29 +0300

    5. weeks ended

---
+++ GameProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManeger gamerManeger = new GamerManeger(new UserValidationManager());
+            gamerManeger.Add(new Gamer 
+            { 
+                Id = 1, 
+                BirthYear = 1998, 
+                FirstName = "BARIŞ", 
+                LastName = "TÜRKMEN", 
+                IdentityNumber = 123456 
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
